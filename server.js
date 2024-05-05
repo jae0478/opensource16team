bdae45cf7d384f381ed34c01e523b636d1809a93
geonwoolim <geonco0709@gmail.com>
2024-05-06 00:09:50 +0900

    크롤링작업중

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({extended : true}));
 // 웹사이트에서 데이터 추출하기 위해
 const axios = require('axios');
 const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 
 // ejs 파일(html 내에서 js 문법 사용 가능)
 app.set('view engine', 'ejs');
@@ -21,57 +22,46 @@ app.listen(8080, function() {
     console.log('listening on 8080')
 });
 
-const puppeteer = require('puppeteer');
-
-// Puppeteer를 사용하여 웹 페이지에서 데이터를 가져오는 함수
-async function fetchDataFromWebsite(url, buttonSelector, resultElementSelector) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
+async function fetchData() {
     try {
-        // 특정 사이트의 URL로 이동
-        await page.goto(url);
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://www.hufs.ac.kr/hufs/11319/subview.do');
+        const content = await page.content();
 
-        // 버튼을 찾아서 클릭
-        await page.click(buttonSelector);
+        const $ = cheerio.load(content);
+        const dataArray = [];
 
-        // 클릭 후 나타나는 데이터가 로드될 때까지 대기 (예: AJAX 요청 완료)
-        await page.waitForSelector(resultElementSelector);
+        const path = '#menuTableDiv > table > tbody > tr > td > ul > li'
+        const data = $(path);
+        data.each((index, list) => {
+            const text = $(list).text();
+            console.log('text is ', text)
+        })
 
-        // 클릭 후 나타나는 데이터 추출
-        const data = await page.evaluate((resultElementSelector) => {
-            const resultElement = document.querySelector(resultElementSelector);
-            return resultElement.textContent.trim(); // 혹은 다른 속성을 추출할 수 있음
-        }, resultElementSelector);
-        console.log(data);
-        return data;
-    } catch (error) {
-        console.error('Error fetching data:', error);
+        return dataArray;
+    } catch(error) {
+        console.error('Error fetching data: ', error);
         return null;
-    } finally {
-        // 브라우저 닫기
-        await browser.close();
     }
 }
-
 
 // /로 들어오면 index.ejs 보내줌
 app.get('/', function(요청, 응답) {
     응답.render('index.ejs');
 });
-// /menu로 들어오면 menu.ejs 보내줌(data 포함)
+
+// /menu로 들어오면 menu.ejs 보내줌
 app.get('/menu', async (req, res) => {
     try {
-        // 데이터 가져오기
-        const data = await fetchDataFromWebsite('http://coop.hufs.ac.kr/sub/welfare_02.php', '#tdh203', 'body > form > table > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr:nth-child(2) > td');
-
-        // 데이터를 menu.ejs에 렌더링하여 응답
+        const data = await fetchData();
         res.render('menu.ejs', { data });
     } catch (error) {
         console.error('Error rendering menu:', error);
         res.status(500).send('Internal Server Error');
     }
 });
+
 // /login으로 들어오면 login.ejs 보내줌
 app.get('/login', function(요청, 응답) {
     응답.render('login.ejs');
