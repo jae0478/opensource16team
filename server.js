13486dbb1ecfef0d62a271c490eaa51732453437
geonwoolim <geonco0709@gmail.com>
2024-05-12 22:36:47 +0900

    - 매주 일요일 01시에 db 메뉴 업데이트 되도록 구현 완료 - 오늘이 무슨 요일인지에 따라 메뉴를 다르게 렌더링하는 기능 구현 완료

--- server.js
+++ server.js
@@ -18,14 +18,17 @@ app.use('/public', express.static('public'));
 // mongoDB를 쓰겠다.
 const MongoClient = require('mongodb').MongoClient;
 // ObjectId() 안에 담기 위해서
-const {ObjectId} = require('mongodb');
+const { ObjectId } = require('mongodb');
+
+// 자동화를 위해 cron을 쓰겠다
+const cron = require('node-cron');
 
 // 환경 변수 사용을 위해
 require('dotenv').config();
 
 var db;
 // mongoDB 연결
-MongoClient.connect(process.env.DB_URL, { useUnifiedTopology: true }, function(에러, client) {
+MongoClient.connect(process.env.DB_URL, { useUnifiedTopology: true }, function (에러, client) {
     if (에러) return console.log(에러);
     db = client.db('oss_project');
     // 서버 열기
@@ -45,40 +48,55 @@ async function student_restaurant() {
     const menu_list = parseMenuContent(content);
 
     await browser.close();
-
-    return menu_list;
+    // db 업데이트
+    db.collection('stu_res').updateOne({ _id: 1 }, { $set: { stu_res: menu_list, _id: 1 } }, function (에러, 결과) {
+        console.log("업데이트 완료");
+    })
 }
 
 // 후생관 교직원식당
 async function professor_restaurant() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.hufs.ac.kr/hufs/11319/subview.do?enc=Zm5jdDF8QEB8JTJGY2FmZXRlcmlhJTJGaHVmcyUyRjIlMkZ2aWV3LmRvJTNGeWVhciUzRDIwMjQlMjZtb250aCUzRDA1JTI2c2VsRGF0ZSUzRDIwMjQwNTA3JTI2c2VsQ2FmSWQlM0RoMjAyJTI2');
+    await page.goto('https://www.hufs.ac.kr/hufs/11319/subview.do');
+
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click("#menu11319_obj34 > div.wrap-food > div.row-tab > ul > li:nth-child(2) > a"),
+    ])
+
     const content = await page.content();
-
     const menu_list = parseMenuContent(content);
-
     await browser.close();
 
-    return menu_list;
+    // db 업데이트
+    db.collection('prof_res').updateOne({ _id: 1 }, { $set: { prof_res: menu_list, _id: 1 } }, function (에러, 결과) {
+        console.log("업데이트 완료");
+    })
 }
 
 // HufsDorm 식당
 async function HufsDorm_restaurant() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.hufs.ac.kr/hufs/11319/subview.do?enc=Zm5jdDF8QEB8JTJGY2FmZXRlcmlhJTJGaHVmcyUyRjIlMkZ2aWV3LmRvJTNGeWVhciUzRDIwMjQlMjZtb250aCUzRDA1JTI2c2VsRGF0ZSUzRDIwMjQwNTA3JTI2c2VsQ2FmSWQlM0RoMjA1JTI2');
+    await page.goto('https://www.hufs.ac.kr/hufs/11319/subview.do');
+
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click("#menu11319_obj34 > div.wrap-food > div.row-tab > ul > li:nth-child(3) > a"),
+    ])
     const content = await page.content();
-
-    const menu_list = parseMenuContent(content);
-
+    const menu_list = parseMenuContentForHufsDorm(content);
     await browser.close();
 
-    return menu_list;
+    // db 업데이트
+    db.collection('hufs_res').updateOne({ _id: 1 }, { $set: { hufs_res: menu_list, _id: 1 } }, function (에러, 결과) {
+        console.log("업데이트 완료");
+    })
 }
 
+// 학생식당, 교직원식당 데이터 추출
 function parseMenuContent(content) {
-    // 데이터 추출하기 위해
     const cheerio = require('cheerio');
     const $ = cheerio.load(content);
 
@@ -86,25 +104,62 @@ function parseMenuContent(content) {
     const menu_data = $(menu_path);
 
     const menu_list = [];
-    menu_data.each((index, list) => {
+    for (var i = 0; i < 7; i++) {
         const sub_menu_list = [];
         // td의 내용이 메뉴없음이면 그 텍스트 출력
-        if ($(list).text() === "등록된 메뉴가없습니다.") {
-            sub_menu_list.push($(list).text());
+        if ($(menu_data[i]).text() === "등록된 메뉴가없습니다.") {
+            sub_menu_list.push($(menu_data[i]).text());
             // td에 바로 내용이 있는게 아니면
         } else {
             // li텍스트만
-            $(list).find('li').each(function () {
+            $(menu_data[i]).find('li').each(function () {
                 const text = $(this).text().trim();
                 if (text !== "")
                     sub_menu_list.push(text);
             });
         }
         menu_list.push(sub_menu_list);
-    });
-
+    }
     return menu_list;
 }
+// 기숙사식당 데이터 추출
+function parseMenuContentForHufsDorm(content) {
+    const cheerio = require('cheerio');
+    const $ = cheerio.load(content);
+
+    const menu_path = '#menuTableDiv > table > tbody > tr > td';
+    const menu_data = $(menu_path);
+
+    const menu_list = [];
+    for (var i = 7; i < 14; i++) {
+        const sub_menu_list = [];
+        // td의 내용이 메뉴없음이면 그 텍스트 출력
+        if ($(menu_data[i]).text() === "등록된 메뉴가없습니다.") {
+            sub_menu_list.push($(menu_data[i]).text());
+            // td에 바로 내용이 있는게 아니면
+        } else {
+            // li텍스트만
+            $(menu_data[i]).find('li').each(function () {
+                const text = $(this).text().trim();
+                if (text !== "")
+                    sub_menu_list.push(text);
+            });
+        }
+        menu_list.push(sub_menu_list);
+    }
+    return menu_list;
+}
+
+// 매주 일요일 01시에 db(메뉴) 업데이트
+cron.schedule('0 1 * * 0', async () => {
+    // 여기에 실행하려는 코드를 넣으세요
+    await student_restaurant(); // 후생관 학생식당
+    await professor_restaurant();   // 후생관 교직원식당
+    await HufsDorm_restaurant();   // HufsDorm 식당
+    console.log('매주 일요일 01시에 실행됩니다.');
+}, {
+    timezone: "Asia/Seoul" // 시간대 설정 (예: 서울 시간대)
+});
 
 // /로 들어오면 index.ejs 보내줌
 app.get('/', function (요청, 응답) {
@@ -113,13 +168,23 @@ app.get('/', function (요청, 응답) {
 
 // /menu로 들어오면 menu.ejs 보내줌
 app.get('/menu', async (req, res) => {
-    const stu_res = await student_restaurant(); // 후생관 학생식당
-    const prof_res = await professor_restaurant();   // 후생관 교직원식당
-    const hufs_res = await HufsDorm_restaurant();   // HufsDorm 식당
+    // 오늘 요일에 따라 db에서 오늘의 메뉴를 가져옴
+    const today = new Date();
+    const dayOfWeek = today.getDay();
 
-    // 매주 월요일 06시에 데이터 변경해서 db에 저장 -> 오늘이 만약 화요일이면 특정 데이터를 db에서 뽑아서 menu.ejs로 보내줌
-    // 화요일 : stu_res->2, 9, 16 | prof_res->2, 9 | hufs_res->2, 9, 16, 23, 30, 37
-    res.render('menu.ejs', { "stu_res": stu_res, "prof_res": prof_res, "hufs_res": hufs_res });
+    let today_stu_res;
+    let today_prof_res;
+    let today_hufs_res;
+
+    const stu_res_result = await db.collection('stu_res').findOne({ _id: 1 });
+    const prof_res_result = await db.collection('prof_res').findOne({ _id: 1 });
+    const hufs_res_result = await db.collection('hufs_res').findOne({ _id: 1 });
+
+    today_stu_res = stu_res_result.stu_res[dayOfWeek];
+    today_prof_res = prof_res_result.prof_res[dayOfWeek];
+    today_hufs_res = hufs_res_result.hufs_res[dayOfWeek];
+
+    res.render('menu.ejs', { "stu_res": today_stu_res, "prof_res": today_prof_res, "hufs_res": today_hufs_res });
 });
 
 // /login으로 들어오면 login.ejs 보내줌
